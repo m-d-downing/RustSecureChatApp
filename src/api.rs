94141f785ce3c97bbd1090b0af757d8fbeebbe73
Michael Downing <michael.d.downing@protonmail.com>
2022-05-14 13:04:36 -0700

    Moved API code to module for readability and added getting user_id from env variable

---
+++ src/api.rs
@@ -0,0 +1,124 @@
+use reqwest::StatusCode;
+
+use crate::Message;
+use crate::Messages;
+use crate::User;
+use crate::Users;
+
+use std::collections::HashMap;
+
+pub fn get_available_users(available_users: &mut Vec<User>) {
+    match reqwest::blocking::get("https://0ibh96tdhk.execute-api.us-west-2.amazonaws.com/users") {
+        Ok(response) => {
+            if response.status() == reqwest::StatusCode::OK {
+                match response.json::<Users>() {
+                    Ok(users) => {
+                        *available_users = users.users;
+                    }
+                    Err(e) => println!("{:?}", e),
+                }
+            }
+        }
+        Err(_) => println!("Could not make request"),
+    }
+}
+
+pub fn send_message(message: &str, sender: &str, recipient: &str) -> bool {
+    let mut map = HashMap::new();
+
+    map.insert("sender", sender);
+    map.insert("recipient", recipient);
+    map.insert("message", message);
+
+    let client = reqwest::blocking::Client::new();
+
+    match client
+        .post("https://0ibh96tdhk.execute-api.us-west-2.amazonaws.com/message")
+        .json(&map)
+        .send()
+    {
+        Ok(response) => {
+            if response.status() == StatusCode::OK {
+                println!("Fetched");
+                true
+            } else {
+                println!("Fetched, but fucked");
+                false
+            }
+        }
+        Err(_) => {
+            println!("Failed to update status");
+            false
+        }
+    }
+}
+
+pub fn get_messages(sender: &str, recipient: &str) -> Vec<Message> {
+    let mut map = HashMap::new();
+
+    map.insert("sender", sender);
+    map.insert("recipient", recipient);
+
+    let client = reqwest::blocking::Client::new();
+
+    match client
+        .post("https://0ibh96tdhk.execute-api.us-west-2.amazonaws.com/getmessages")
+        .json(&map)
+        .send()
+    {
+        Ok(response) => {
+            if response.status() == StatusCode::OK {
+                match response.json::<Messages>() {
+                    Ok(data) => {
+                        println!("{:?}", data.messages);
+                        return data.messages;
+                    }
+                    Err(_) => todo!(),
+                }
+            }
+            return Vec::new();
+        }
+        Err(_) => todo!(),
+    }
+}
+
+pub fn set_user_status(status: &str, user: &mut Option<User>) -> bool {
+    match user {
+        Some(usr) => {
+            let mut map = HashMap::new();
+            let user_status = String::from(status);
+
+            map.insert("user_id", &usr.user_id);
+            map.insert("status", &user_status);
+
+            let client = reqwest::blocking::Client::new();
+
+            match client
+                .post("https://0ibh96tdhk.execute-api.us-west-2.amazonaws.com/status")
+                .json(&map)
+                .send()
+            {
+                Ok(response) => {
+                    if response.status() == StatusCode::OK {
+                        println!("Fetched");
+                        match response.json::<User>() {
+                            Ok(response_user) => {
+                                *user = Some(response_user);
+                            }
+                            Err(e) => println!("{:?}", e),
+                        }
+                        true
+                    } else {
+                        println!("Server Error");
+                        false
+                    }
+                }
+                Err(_) => {
+                    println!("Failed to update status");
+                    false
+                }
+            }
+        }
+        None => return false,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,32 @@
+mod api;
+
 use eframe::{egui, egui::CentralPanel, egui::Context, egui::Layout, epi::App, epi::Frame};
-use reqwest::StatusCode;
 use serde::Deserialize;
 use std::{
-    collections::HashMap,
-    sync::mpsc::{channel, Receiver, Sender}, time::SystemTime,
+    env,
+    sync::mpsc::{channel, Receiver, Sender},
+    time::{SystemTime, UNIX_EPOCH},
 };
-use std::time::SystemTime;
 
 #[derive(Deserialize, Clone)]
-struct User {
+pub struct User {
     user_id: String,
     user_name: String,
 }
 
 #[derive(Deserialize)]
-struct Users {
+pub struct Users {
     users: Vec<User>,
 }
 #[derive(Deserialize, Clone, Debug)]
 
-struct Message {
+pub struct Message {
     message: String,
     sent_at: String,
 }
 #[derive(Deserialize)]
 
-struct Messages {
+pub struct Messages {
     messages: Vec<Message>,
 }
 
@@ -59,7 +60,8 @@ struct SecureChatApp {
 impl Default for SecureChatApp {
     fn default() -> Self {
         let (send_messages, recv_messages) = channel();
-        Self{message: String::new(),
+        Self {
+            message: String::new(),
             chat_history: String::new(),
             state: AppState::default(),
             user: None,
@@ -67,120 +69,21 @@ impl Default for SecureChatApp {
             users_fetched: false,
             available_users: Vec::new(),
             chatting_with: String::new(),
-            messages:Vec::new(),
-            sent:Vec::new(),
-            send_messages, recv_messages}
-    }
-}
-
-fn get_available_users(available_users: &mut Vec<User>) {
-    match reqwest::blocking::get("https://0ibh96tdhk.execute-api.us-west-2.amazonaws.com/users") {
-        Ok(response) => {
-            if response.status() == reqwest::StatusCode::OK {
-                match response.json::<Users>() {
-                    Ok(users) => {
-                        *available_users = users.users;
-                    }
-                    Err(e) => println!("{:?}", e),
-                }
-            }
-        }
-        Err(_) => println!("Could not make request"),
-    }
-}
-
-fn send_message(message: &str, sender: &str, recipient: &str) -> bool {
-    let mut map = HashMap::new();
-
-    map.insert("sender", sender);
-    map.insert("recipient", recipient);
-    map.insert("message", message);
-
-    let client = reqwest::blocking::Client::new();
-
-    match client
-        .post("https://0ibh96tdhk.execute-api.us-west-2.amazonaws.com/message")
-        .json(&map)
-        .send()
-    {
-        Ok(response) => {
-            if response.status() == StatusCode::OK {
-                println!("Fetched");
-                true
-            } else {
-                println!("Fetched, but fucked");
-                false
-            }
-        }
-        Err(_) => {
-            println!("Failed to update status");
-            false
-        }
-    }
-}
-
-fn get_messages(sender: &str, recipient: &str) -> Vec<Message> {
-    let mut map = HashMap::new();
-
-    map.insert("sender", sender);
-    map.insert("recipient", recipient);
-
-    let client = reqwest::blocking::Client::new();
-
-    match client
-        .post("https://0ibh96tdhk.execute-api.us-west-2.amazonaws.com/getmessages")
-        .json(&map)
-        .send()
-    {
-        Ok(response) => {
-            if response.status() == StatusCode::OK {
-                match response.json::<Messages>() {
-                    Ok(data) => return data.messages,
-                    Err(_) => todo!(),
-                }
-            }
-            return Vec::new();
-        }
-        Err(_) => todo!(),
-    }
-}
-
-fn set_user_status(status: &str, user: &mut Option<User>) -> bool {
-    let mut map = HashMap::new();
-
-    map.insert("user_id", "5cc0f84a-35b0-4331-8798-47013f9eeb40");
-    map.insert("status", status);
-
-    let client = reqwest::blocking::Client::new();
-
-    match client
-        .post("https://0ibh96tdhk.execute-api.us-west-2.amazonaws.com/status")
-        .json(&map)
-        .send()
-    {
-        Ok(response) => {
-            if response.status() == StatusCode::OK {
-                println!("Fetched");
-                match response.json::<User>() {
-                    Ok(response_user) => {
-                        *user = Some(response_user);
-                    }
-                    Err(e) => println!("{:?}", e),
-                }
-                true
-            } else {
-                println!("Server Error");
-                false
-            }
-        }
-        Err(_) => {
-            println!("Failed to update status");
-            false
+            messages: Vec::new(),
+            sent: Vec::new(),
+            send_messages,
+            recv_messages,
         }
     }
 }
 
 impl SecureChatApp {
+    fn new(user_id: String, user_name: String) -> SecureChatApp {
+        let mut app = SecureChatApp::default();
+        app.user = Some(User { user_id, user_name });
+        return app;
+    }
+
     fn render_chat(&mut self, ctx: &Context) {
         if self.count % 200 == 0 {
             match &self.user {
@@ -190,28 +93,27 @@ impl SecureChatApp {
                     let recipient_id = self.chatting_with.clone();
                     std::thread::spawn(move || {
                         let messages =
-                            get_messages(&sender_id.as_str(), &recipient_id.as_str());
+                            api::get_messages(&sender_id.as_str(), &recipient_id.as_str());
                         send.send(messages).expect("Whoops!");
                     });
                 }
-             None => todo!(),
+                None => todo!(),
             }
-        }    
-                if let Ok(response) = self.recv_messages.try_recv() {
-            
-                            self.messages = response;
-                            for message in &mut self.messages {
-                                let new_string = message.message.clone()
-                                    + " "
-                                    + message.sent_at.as_str()
-                                    + "\n";
-                                self.chat_history += new_string.as_str();
-                                
-                            }
-                        
-                    
-                }
-        
+        }
+        if let Ok(mut response) = self.recv_messages.try_recv() {
+            let mut sent_messages: Vec<Message> = self.sent.clone();
+            response.append(&mut sent_messages);
+            response.sort_by(|a, b| a.sent_at.cmp(&b.sent_at));
+            println!("{:?}", response);
+            self.messages = response;
+
+            self.chat_history = String::new();
+            for message in &mut self.messages {
+                let new_string = message.message.clone() + " " + message.sent_at.as_str() + "\n";
+                self.chat_history += new_string.as_str();
+            }
+        }
+
         CentralPanel::default().show(ctx, |ui| {
             ui.with_layout(Layout::bottom_up(eframe::egui::Align::LEFT), |ui| {
                 let text_response = ui.add(
@@ -226,7 +128,7 @@ impl SecureChatApp {
 
                     match &self.user {
                         Some(user) => {
-                            send_message(
+                            api::send_message(
                                 self.message.as_str(),
                                 user.user_id.as_str(),
                                 self.chatting_with.as_str(),
@@ -236,7 +138,14 @@ impl SecureChatApp {
                             println!("No User");
                         }
                     }
-                    self.sent.push(Message{message:self.message, sent_at: SystemTime::now() });
+                    self.sent.push(Message {
+                        message: self.message.clone(),
+                        sent_at: SystemTime::now()
+                            .duration_since(UNIX_EPOCH)
+                            .expect("Time Error")
+                            .as_millis()
+                            .to_string(),
+                    });
                     self.message.push('\n');
                     self.chat_history += &self.message;
                     self.message.clear();
@@ -254,7 +163,7 @@ impl SecureChatApp {
                 ui.add_space(200.0);
                 let response = ui.add_sized([200.0, 50.0], egui::Button::new("Login"));
                 if response.clicked() {
-                    if set_user_status("signedin", &mut self.user) {
+                    if api::set_user_status("signedin", &mut self.user) {
                         self.state = AppState::RenderLobby
                     }
                 }
@@ -287,7 +196,7 @@ impl SecureChatApp {
                         ui.add_space(100.0);
 
                         if ui.button("Log Out").clicked() {
-                            if set_user_status("signedout", &mut self.user) {
+                            if api::set_user_status("signedout", &mut self.user) {
                                 self.state = AppState::RenderLobby;
                             }
                         }
@@ -298,7 +207,7 @@ impl SecureChatApp {
                 }
             } else {
                 ui.heading("Loading");
-                get_available_users(&mut self.available_users);
+                api::get_available_users(&mut self.available_users);
                 self.users_fetched = true
             }
         });
@@ -321,7 +230,19 @@ impl App for SecureChatApp {
 }
 
 fn main() {
-    let app = SecureChatApp::default();
+    for (n, v) in env::vars() {
+        println!("{}: {}", n, v);
+    }
+    let user_id = match env::var_os("CAPSTONE_CHAT_ID") {
+        Some(v) => v.into_string().unwrap(),
+        None => panic!("$USER is not set"),
+    };
+    let user_name = match env::var_os("CAPSTONE_CHAT_NAME") {
+        Some(v) => v.into_string().unwrap(),
+        None => panic!("$USER is not set"),
+    };
+
+    let app = SecureChatApp::new(user_id, user_name);
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(Box::new(app), native_options);
 }
